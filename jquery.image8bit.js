0dc7c24ce4a21c7a0b69389eda164ab6271ee46b
Guillaume Marty <edo999@gmail.com>
2010-09-17 11:00:04 +0200

    Inlined RGBtoHSL

--- jquery.image8bit.js
+++ jquery.image8bit.js
@@ -2,6 +2,9 @@
  * jQuery plugin to give images the aspect of good old 8-bit graphics.
  * The original img tag is replaced by a canvas tag, thus breaking jQuery fluent interface.
  * 
+ * RGB to HSL conversion taken from:
+ * http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+ * 
  * How to use?
  * $("img").image8bit({blocksize : 5, granularity : 16});
  * 
@@ -13,45 +16,10 @@
  * @todo: Externalize canvas support test to allow using libraries like explorercanvas.
  * @todo: Apply reusable attributes (style, id, class...) from the original img to the canvas.
  * @todo: use jQuery load() to ensure the image is loaded before processing (required?).
- * @todo: Inlining rgbToHsl() may reduce size after minification significantly. 
  */
 (function($, document, sCanvas, s2d) {
 	$.fn.image8bit = function(params) {
-		/**
-		 * Converts an RGB color value to HSL. Conversion formula adapted from
-		 * http://en.wikipedia.org/wiki/HSL_color_space. Assumes r, g, and b are
-		 * contained in the set [0, 255] and returns h, s, and l in the set [0, 1].
-		 * http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-		 * 
-		 * @param {number} r The red color value.
-		 * @param {number} g The green color value.
-		 * @param {number} b The blue color value.
-		 * @return {Array.<number>} The HSL representation.
-		 */
-		function rgbToHsl(r, g, b) {
-			r /= 255;
-			g /= 255;
-			b /= 255;
-			var max = Math.max(r, g, b), min = Math.min(r, g, b), h = 0, s = 0, l = (max + min) / 2, d;
-			if (max !== min) {
-				d = max - min;
-				s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-				switch (max) {
-				case r:
-					h = (g - b) / d + (g < b ? 6 : 0);
-					break;
-				case g:
-					h = (b - r) / d + 2;
-					break;
-				default:
-					h = (r - g) / d + 4;
-					break;
-				}
-				h /= 6;
-			}
-			return [ h, s, l ];
-		}
-
+		// Check for <canvas> support first.
 		if (document.createElement(sCanvas).getContext) {
 			params = $.extend({
 				blocksize : 5,
@@ -71,7 +39,13 @@
 					blockSizeY,
 					y,
 					x,
-					data;
+					color,
+					max,
+					min,
+					h,
+					s,
+					l,
+					d;
 				canvas.width = width;
 				canvas.height = height;
 				context = canvas.getContext(s2d);
@@ -86,14 +60,45 @@
 						if (blockSizeY + y > height) {
 							blockSizeY = height - y;
 						}
+
+						// Draw block to a 1x1 px tmp canvas element to extract mean color.
 						pixelContext.drawImage(image, x, y, blockSizeX, blockSizeY, 0, 0, 1, 1);
-						data = pixelContext.getImageData(0, 0, 1, 1).data;
-						data = rgbToHsl(data[0], data[1], data[2]);
-						data[0] = Math.round(data[0] / params.granularity * 100) * params.granularity * 3.6;
-						data[1] = Math.round(data[1] / params.granularity * 100) * params.granularity+"%";
-						data[2] = Math.round(data[2] / params.granularity * 100) * params.granularity;
-						context.fillStyle = "hsl(" + data.join(",") + "%)";
-						
+						try {
+							color = pixelContext.getImageData(0, 0, 1, 1).data;
+						} catch(err) {
+							window.console && window.console.log && console.log(err.message);
+							return;
+						}
+
+						// Convert color from RGB to HSL.
+						color = [color[0]/255, color[1]/255, color[2]/255];
+						max = Math.max(color[0], color[1], color[2]);
+						min = Math.min(color[0], color[1], color[2]);
+						h = s = 0;
+						l = (max + min) / 2;
+						if (max !== min) {
+							d = max - min;
+							s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+							switch (max) {
+							case color[0]:
+								h = (color[1] - color[2]) / d + (color[1] < color[2] ? 6 : 0);
+								break;
+							case color[1]:
+								h = (color[2] - color[0]) / d + 2;
+								break;
+							default:
+								h = (color[0] - color[1]) / d + 4;
+								break;
+							}
+							h /= 6;
+						}
+						color = [h, s, l];
+
+						// Apply granularity.
+						color[0] = Math.round(color[0] / params.granularity * 100) * params.granularity * 3.6;
+						color[1] = Math.round(color[1] / params.granularity * 100) * params.granularity + "%";
+						color[2] = Math.round(color[2] / params.granularity * 100) * params.granularity;
+						context.fillStyle = "hsl(" + color.join(",") + "%)";
 						context.fillRect(x, y, params.blocksize, params.blocksize);
 					}
 				}
